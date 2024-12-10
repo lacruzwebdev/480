05fbb3687d1d6a36d94585fa8f2bc61039d3b97f
Ignacio Lacruz <hola@lacruzwebdev.com>
2024-12-10 18:26:21 +0100

    Add LoginForm

---
+++ src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,60 @@
+import styles from "./LoginForm.module.css"
+import Button from "../Button"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import useAuth from "../../hooks/useAuth"
+import Spinner from "../Spinner"
+import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+import { useTranslation } from "react-i18next"
+import FormInput from "../FormInput"
+
+const formSchema = z.object({
+  email: z.string().email("Email no válido"),
+  password: z.string(),
+})
+
+export default function LoginForm() {
+  const [authError, setAuthError] = useState("")
+  const navigate = useNavigate()
+  const { t } = useTranslation()
+  const { login, isLoading } = useAuth()
+  const {
+    getValues,
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+
+  async function handleLogin() {
+    setAuthError("")
+    const auth = await login(getValues("email"), getValues("password"))
+    if ("error" in auth) {
+      setAuthError(auth.error)
+      return
+    }
+    if (auth.id) {
+      navigate("/dashboard")
+    }
+  }
+
+  return (
+    <div className={styles.Wrapper}>
+      {authError && <p className={styles.AuthError}>{t("authError")}</p>}
+      <form onSubmit={handleSubmit(handleLogin)}>
+        <FormInput label={t("email")} name="email" register={register} errors={errors} />
+        <FormInput label={t("password")} name="password" register={register} errors={errors} />
+        <Button className={styles.Button}>
+          {isLoading ? <Spinner color="#FFFFFF" /> : "Login"}
+        </Button>
+      </form>
+    </div>
+  )
+}
