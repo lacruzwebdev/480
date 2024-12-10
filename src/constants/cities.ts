a3a524cab2fe27f8d83bc45c56249adde9c3a58b
Ignacio Lacruz <hola@lacruzwebdev.com>
2024-12-10 17:45:41 +0100

    Add constants

---
+++ src/constants/cities.ts
@@ -0,0 +1,1 @@
+export const CITIES = ["London", "Toronto", "Singapur"]
---
+++ src/constants/languages.ts
@@ -0,0 +1,6 @@
+export const LANGUAGES = [
+  { id: "es", name: "Español" },
+  { id: "en", name: "English" },
+]
+
+export const DEFAULT_LANGUAGE = "en"
