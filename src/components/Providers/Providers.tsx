697b1f07ade49af8775f6ecaaec03f59c1c20beb
Ignacio Lacruz <hola@lacruzwebdev.com>
2024-12-10 17:56:32 +0100

    Add Tanstack Query Provider

--- src/components/Providers/Providers.tsx
+++ src/components/Providers/Providers.tsx
@@ -1,7 +1,12 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import AuthProvider from "../../providers/AuthProvider"
+
+const queryClient = new QueryClient()
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
+    <QueryClientProvider client={queryClient}>
         <AuthProvider>{children}</AuthProvider>
+    </QueryClientProvider>
   )
 }
